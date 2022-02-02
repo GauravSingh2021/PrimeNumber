7d0491ff28423385e5cf66e6337cc855ad70dd34
Gauravjisingh <gaurav7084578879@gmail.com>
2022-02-02 14:49:50 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System; 
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number:");
+            int num=int.Parse(Console.ReadLine());
+            int count=0;
+
+            for (int i = 1 ; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                    
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine(num + " is Prime Number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is Not prime Number");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
